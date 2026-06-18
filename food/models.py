11949d1c2ec0b407248bf5beceb2053631860844
Prabhat Burnwal <prabhat.burnwal@bitbasegroup.in>
2026-06-18 23:04:30 +0530

    Food menu list page done.

--- food/models.py
+++ food/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
 	item_name = models.CharField(max_length=200)
 	item_price = models.DecimalField(max_digits=6, decimal_places=2)
 	item_description = models.TextField()
+	item_image = models.CharField(max_length=500, default='https://storage.googleapis.com/ds-builder-bucket/000_menu_placeholder.png')
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
 
--- food/views.py
+++ food/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Item
 
@@ -6,7 +6,13 @@ from .models import Item
 
 def index(request):
 	items = Item.objects.all()
-	return HttpResponse(items)
+
+	context = {'items': items}
+	return render(request, "food/index.html", context)
+
+def detail(request, item_id):
+	item = get_object_or_404(Item, id=item_id)
+	return render(request, "food/detail.html", {"item": item})
 
 def add_food(request):
 	return HttpResponse("<h1 style='text-align: center; color: lightblue;'>Add Food</h1>")
